9aea8476129529e6099357093ef7059c08d1df85
Takuma Yoshimi <ysm0706glee@gmail.com>
2024-01-24 17:06:14 +0900

    add calendar page

--- app/routes/calendar._index.tsx
+++ app/routes/calendar._index.tsx
@@ -1,7 +1,21 @@
+import { DatePicker, type DateValue } from "@mantine/dates";
+import { useNavigate } from "@remix-run/react";
+
 export default function Calendar() {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>Calendar</h1>
+      <DatePicker
+        onChange={(newDate: DateValue) => {
+          if (!newDate) return;
+          const year = newDate.getFullYear();
+          const month = (newDate.getMonth() + 1).toString().padStart(2, "0");
+          const day = newDate.getDate().toString().padStart(2, "0");
+          const formattedDate = `${year}-${month}-${day}`;
+          navigate(`/calendar/workout_menus?date=${formattedDate}`);
+        }}
+      />
     </div>
   );
 }
