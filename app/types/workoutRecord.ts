e231bb43790f5aa4ff6f092b556420e881fdd6df
Takuma Yoshimi <ysm0706glee@gmail.com>
2024-01-24 17:20:06 +0900

    add workoutRecord type

---
+++ app/types/workoutRecord.ts
@@ -0,0 +1,16 @@
+// TODO: refactor
+export type WorkoutRecord = {
+  id?: number;
+  sets: number;
+  reps: number;
+  weight: number;
+};
+
+export type ExerciseRecord = {
+  id: number;
+  records: WorkoutRecord[];
+};
+
+export type WorkoutRecords = {
+  [exerciseName: string]: ExerciseRecord;
+};
