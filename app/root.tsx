f3eb0217ce8326d22c5ae3562b28e85225133bdf
Takuma Yoshimi <ysm0706glee@gmail.com>
2024-03-10 17:34:49 +0100

    refactor supabase

--- app/root.tsx
+++ app/root.tsx
@@ -11,15 +11,91 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useRevalidator,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
-import type { LinksFunction } from "@vercel/remix";
+import type { LinksFunction, LoaderFunctionArgs } from "@vercel/remix";
+
+import {
+  createBrowserClient,
+  createServerClient,
+  parse,
+  serialize,
+} from "@supabase/ssr";
+import { useEffect, useState } from "react";
+import { Database } from "./types/supabase";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY)
+    throw new Error(
+      "SUPABASE_URL and SUPABASE_ANON_KEY must be defined in .env"
+    );
+  const env = {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  };
+  const cookies = parse(request.headers.get("Cookie") ?? "");
+  const headers = new Headers();
+  const supabase = createServerClient<Database>(
+    env.SUPABASE_URL,
+    env.SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(key) {
+          return cookies[key];
+        },
+        set(key, value, options) {
+          headers.append("Set-Cookie", serialize(key, value, options));
+        },
+        remove(key, options) {
+          headers.append("Set-Cookie", serialize(key, "", options));
+        },
+      },
+    }
+  );
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return {
+    env,
+    session,
+    headers,
+  };
+}
+
 export default function App() {
+  const { env, session } = useLoaderData<typeof loader>();
+  const { revalidate } = useRevalidator();
+
+  const [supabase] = useState(() =>
+    createBrowserClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
+  );
+
+  const serverAccessToken = session?.access_token;
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (
+        event !== "INITIAL_SESSION" &&
+        session?.access_token !== serverAccessToken
+      ) {
+        // server and client are out of sync.
+        revalidate();
+      }
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [serverAccessToken, supabase, revalidate]);
+
   return (
     <html lang="en">
       <head>
@@ -31,7 +107,7 @@ export default function App() {
       </head>
       <body>
         <MantineProvider>
-          <Outlet />
+          <Outlet context={{ supabase }} />
           <ScrollRestoration />
           <Scripts />
           <LiveReload />
