d76cd5ff0f227b0a43de9ee72726e7f483494b0a
Takuma Yoshimi <ysm0706glee@gmail.com>
2024-04-04 17:52:39 +0200

    refactor

--- app/lib/records.ts
+++ app/lib/records.ts
@@ -1,8 +1,4 @@
-import {
-  FormDataEntry,
-  ParsedFormData,
-  WorkoutRecords,
-} from "~/types/workoutRecord";
+import { FormDataEntry, ParsedFormData, Records } from "~/types/workoutRecord";
 import { Tables } from "../types/supabase";
 
 export function parseFormData(
@@ -13,13 +9,13 @@ export function parseFormData(
   const records: ParsedFormData[] = [];
   const tempRecords: { [key: string]: ParsedFormData } = {};
   formData.forEach((value) => {
-    const { exercises_id, sets, type } = value;
-    const recordKey = `${exercises_id}-${sets}`;
+    const { exercise_id, sets, type } = value;
+    const recordKey = `${exercise_id}-${sets}`;
     if (!tempRecords[recordKey]) {
       tempRecords[recordKey] = {
-        workout_menus_id: workoutMenuId,
+        menu_id: workoutMenuId,
         date,
-        exercises_id: parseInt(exercises_id, 10),
+        exercise_id: parseInt(exercise_id, 10),
         sets: parseInt(sets, 10),
         reps: 0,
         weight: 0,
@@ -38,22 +34,22 @@ export function parseFormData(
   return records;
 }
 
-type Data = Tables<"workout_records"> & {
+type Data = Tables<"records"> & {
   exercises: Tables<"exercises"> | null;
 };
 
 export function formateRecords(data: Data[]) {
-  const workoutRecords: WorkoutRecords = {};
+  const records: Records = {};
   data.forEach((record) => {
     const name = record.exercises?.name;
     if (name) {
-      if (!workoutRecords[name]) {
-        workoutRecords[name] = {
-          id: record.exercises_id,
+      if (!records[name]) {
+        records[name] = {
+          id: record.exercise_id,
           records: [],
         };
       }
-      workoutRecords[name].records.push({
+      records[name].records.push({
         id: record.id,
         sets: record.sets,
         reps: record.reps,
@@ -61,5 +57,5 @@ export function formateRecords(data: Data[]) {
       });
     }
   });
-  return workoutRecords;
+  return records;
 }
